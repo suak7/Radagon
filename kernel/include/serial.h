598bc3df26285c0d00f6fa9a17995fd6c95ee9e7
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-22 18:55:25 -0500

    add serial output

---
+++ kernel/include/serial.h
@@ -0,0 +1,14 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define COM1 0x3F8
+
+bool serial_init(void);
+void serial_write_char(char c);
+void serial_print(const char* str);
+void serial_print_hex(uint32_t value);
+
+#endif
